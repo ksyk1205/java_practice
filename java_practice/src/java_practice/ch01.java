6d881fe9ebda9b43c640485ee49ebc3f5d984bd6
Gwak Seyeon <Gwak Seyeon@DESKTOP-S28M849>
2021-02-16 00:49:56 +0900

    day1

---
+++ java_practice/src/java_practice/ch01.java
@@ -0,0 +1,26 @@
+package java_practice;
+
+public class ch01 {
+	
+		public static void main(String[] args) {
+		//문제 1-1 )  "첫 java 프로그램입니다." ,"화면에 출력하고 있습니다." 출력하는 프로그램
+		System.out.println("첫 java 프로그램입니다.");
+		System.out.println("화면에 출력하고 있습니다.");
+		//문제 1-2) 화면 연속 표시 하는 방법 
+		System.out.println("첫 java 프로그램입니다. 화면에 출력하고 있습니다.");
+		
+		System.out.print("첫 java 프로그램입니다.");
+		System.out.println("화면에 출력하고 있습니다.");
+		
+		System.out.println("화면에 출력하고 있습니다."+"화면에 출력하고있습니다.");
+		//문제 1-3) 한 줄에 한 글자씩 자신의 이름을 표시하는 프로그램
+		System.out.println("gwak");
+		System.out.println("se");
+		System.out.println("yeon");
+		
+		System.out.println("gwak\nse\nyeon");
+		
+		
+	}
+
+}
