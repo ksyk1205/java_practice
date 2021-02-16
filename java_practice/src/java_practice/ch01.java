e5faa19fecfe8f5c3a5bdd068a26ac9c2dc909cc
Gwak Seyeon <Gwak Seyeon@DESKTOP-S28M849>
2021-02-17 00:17:15 +0900

    day02

--- java_practice/src/java_practice/ch01.java
+++ java_practice/src/java_practice/ch01.java
@@ -1,7 +1,7 @@
 package java_practice;
 
 public class ch01 {
-	
+		//1장 . 화면에 문자 표시하기
 		public static void main(String[] args) {
 		//문제 1-1 )  "첫 java 프로그램입니다." ,"화면에 출력하고 있습니다." 출력하는 프로그램
 		System.out.println("첫 java 프로그램입니다.");
