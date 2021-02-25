fff25175c01200211f35149ac65909db448abd86
Gwak seyeon <ksyk1205@naver.com>
2021-02-25 23:33:12 +0900

    day11

---
+++ java_practice/src/java_practice/ch05.java
@@ -0,0 +1,21 @@
+package java_practice;
+
+public class ch05 {
+	//5장 기본자료형과 연산
+	public static void main(String[] args) {
+		//practice01();
+	}
+	public static void practice01() {
+	// 문제 5-1. 8진수 12, 10진수 12,16진수를 12 를 10진수로 표시
+		System.out.println("8진수 12는 10진수로"+012+"입니다.");
+		System.out.println("10진수 12는 10진수로"+12+"입니다.");
+		System.out.println("16진수 12는 10진수로"+0x12+"입니다.");
+			
+	}
+	public static void practice02() {
+	// 문제 5-2.
+			
+	}
+		
+
+}
