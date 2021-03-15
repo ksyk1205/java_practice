60120370afedc688cb1480f3e12aa4f0efcf80b1
Gwak seyeon <ksyk1205@naver.com>
2021-03-15 23:55:19 +0900

    day02

---
+++ java_practice/src/ch08/human.java
@@ -0,0 +1,12 @@
+package ch08;
+
+public class human {
+	//문제 8-1.이름,신장,체줄 등을 멤버로 가지는'사람 클래스'를 작성하자
+	//사람 클래스 human은 3개의 필드로 구성된다
+	String name; //이름
+	int height;  //신장
+	int weight;  //체중
+	
+	
+
+}
---
+++ java_practice/src/ch08/humanTester.java
@@ -0,0 +1,32 @@
+package ch08;
+
+public class humanTester { //문ㅈㅔ 8-1
+
+	public static void main(String[] args) {
+		//클래스형 변수를 선언 한다.
+		human seyeon = new human();
+		human seyeon2 = new human();
+		
+		//클래스형 변수로 선언한 후 변수명으로 참조하는  인스턴스를 접근할 수 있다.
+		seyeon.name = "세연";
+		seyeon.height = 160;
+		seyeon.weight = 50;
+		
+		seyeon2.name ="세연이";
+		seyeon.height = 165;
+		seyeon.weight = 55;
+		
+		System.out.println("이름 : "+seyeon.name);
+		System.out.println("신장 : "+seyeon.height);
+		System.out.println("몸무게 : "+seyeon.weight);
+		System.out.println();
+		
+		System.out.println("이름 : "+seyeon2.name);
+		System.out.println("신장 : "+seyeon2.height);
+		System.out.println("몸무게 : "+seyeon2.weight);
+		
+		
+
+	}
+
+}
